f477ac095a68f89624ca14f70b5ed37e31cfbc2c
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2022-01-29 13:00:59 +0530

    Update ocr.py

--- examples/async/ocr.py
+++ examples/async/ocr.py
@@ -3,7 +3,7 @@ from openrobot.api_wrapper import AsyncClient
 client = AsyncClient(...)
 
 # From URL:
-ocr = await client.ocr(url=...)
+ocr = await client.ocr(source=...)
 
 # From bytes:
 from io import BytesIO
